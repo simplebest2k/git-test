53afdc4bed79a031ac556fbd814d5f16841cf0e7
hskim <simplebest2k@naver.com>
2021-10-16 16:52:28 +0900

    stash 최종결과 반영

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,7 +1,16 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+@RestController
 public class TestController {
    public Object get2() {
       return "Hello";
    }
+   public Object get() {
+      return new HashMap<String,String>();
+   }
+
 }
